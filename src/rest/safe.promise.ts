3aacbf92765936b3d3ba044dfca6470dd764a418
Nihal Konda <nihal.konda@sjsu.edu>
2020-12-09 16:01:54 -0800

    Quick Commit

--- src/rest/safe.promise.ts
+++ src/rest/safe.promise.ts
@@ -1,22 +1,22 @@
 let handlers = {
-    handleEmptyToken : ()=>{} ,
-    handleExpiredRefreshToken : ()=>{} ,
-    handleNetworkError : ()=>{}
+    handleEmptyToken: () => { },
+    handleExpiredRefreshToken: () => { },
+    handleNetworkError: () => { }
 }
 
-export function setHandlers(value:typeof handlers){
+export function setHandlers(value: typeof handlers) {
     handlers = value;
 }
 
 
-export default function safePromise<T>(promise:Promise<T>):Promise<T>{
-    console.log('safe.promise',promise);
-    return new Promise<T>((resolve,reject)=>{
-        promise.then((result)=>{
-            console.log('safe.promise','result',result);
+export default function safePromise<T>(promise: Promise<T>): Promise<T> {
+    console.log('safe.promise', promise);
+    return new Promise<T>((resolve, reject) => {
+        promise.then((result) => {
+            console.log('safe.promise', 'result', result);
             resolve(result);
-        }).catch((error)=>{
-            console.log('safe.promise','error',error);
+        }).catch((error) => {
+            console.log('safe.promise', 'error', error, '|', error.message, '|', error.status);
             // if(error.message === Strings.ERROR.TOKEN_EMPTY){
             //     console.log('Sign In Required','');
             //     handlers.handleEmptyToken();
